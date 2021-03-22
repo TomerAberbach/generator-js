2eb8b912d6086a40732122c7fc25ec57a91634b2
Tomer Aberbach <tomeraberbach@gmail.com>
2021-03-21 21:15:39 -0400

    test: don't make requests in tests

--- app/helpers/licenses.js
+++ app/helpers/licenses.js
@@ -1,15 +1,18 @@
 const got = require(`got`)
 
-const requestLicenses = async () =>
-  (await got(`https://api.github.com/licenses`).json()).map(
-    ({ name, spdx_id: identifier, url }) => ({
-      name,
-      identifier,
-      requestContent: async name =>
-        (await got(url).json()).body
-          .replace(`[year]`, String(new Date().getFullYear()))
-          .replace(`[fullname]`, name)
-    })
-  )
+const requestLicenses =
+  process.env.NODE_ENV === `test`
+    ? () => Promise.resolve([])
+    : async () =>
+        (await got(`https://api.github.com/licenses`).json()).map(
+          ({ name, spdx_id: identifier, url }) => ({
+            name,
+            identifier,
+            requestContent: async name =>
+              (await got(url).json()).body
+                .replace(`[year]`, String(new Date().getFullYear()))
+                .replace(`[fullname]`, name)
+          })
+        )
 
 module.exports = requestLicenses
